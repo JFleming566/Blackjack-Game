218bb4d2caec489cd8acfcdd2aae0436f4d7898b
Jeremy Fleming <thevisionmachine@gmail.com>
2023-10-19 07:52:03 -0500

    Added emojis

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ let sum = firstCard + secondCard;
 
 // console.log(sum);
 if (sum <= 20) {
-    console.log("Do you want to draw a new card?");
+    console.log("Do you want to draw a new card? 🙂");
 } else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack!");
+    console.log("Wohoo! You've got Blackjack! 🥳");
 } else {
-    console.log("You're out of the game!");
+    console.log("You're out of the game! 😭");
 }
