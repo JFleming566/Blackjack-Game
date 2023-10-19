ccf5ce02e2761ac1500f693073ba0fc9a2c90ab3
Jeremy Fleming <thevisionmachine@gmail.com>
2023-10-18 20:36:27 -0500

    modified if-else statement

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-let firstCard = 2;
-let secondCard = 10;
+let firstCard = 11;
+let secondCard = 11;
+
 let sum = firstCard + secondCard;
 
-console.log(sum);
+// console.log(sum);
+if (sum <= 20) {
+    console.log("Do you want to draw a new card?");
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack!");
+} else {
+    console.log("You're out of the game!");
+}
