050ab8ee345ca68d3c7e5b87a8c67660988d8cf8
Jeremy Fleming <thevisionmachine@gmail.com>
2023-10-22 22:42:24 -0500

    changed card variables to arrays

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let firstCard = 11;
 let secondCard = 5;
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackjack = false;
 let isAlive = true;
@@ -7,9 +8,19 @@ let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEL = document.getElementById("cards-el");
+let gameStart = false;
+let changeButton = document.getElementById("new-card-pre-game");
+
 
 function startGame() {
-    cardsEL.textContent += firstCard + " " + secondCard;
+    gameStart = true;
+    document.getElementById("new-card-pre-game").id = "new-card";
+    renderGame();
+}
+
+function renderGame() {
+    cardsEL.textContent = "Cards: " + cards[0] + " " + cards[1];
+    sumEl.textContent = "Sum: " + sum;
     if (sum <= 20) {
         message = "Do you want to draw a new card?";
     } else if (sum === 21) {
@@ -20,9 +31,13 @@ function startGame() {
         isAlive = false;
     }
     messageEl.textContent = message;
-    sumEl.textContent += sum;
+  
 }
 
 function newCard() {
-    console.log("Drawing a new card from the deck");
+    if (gameStart) {
+    let card = 3;
+    sum += card;
+    renderGame();
+    };
 }
